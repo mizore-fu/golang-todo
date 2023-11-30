f35f620c94bf2fa2144fa15ea902ef567bd06c45
mizore-fu <mhxxname0782@gmail.com>
2023-11-30 20:17:31 +0900

    タスクを取得するエンドポイントを作成

--- app/main.go
+++ app/main.go
@@ -6,12 +6,23 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+type Task struct {
+	ID string `json:"id"`
+	Name string `json:"name"`
+	Completed bool `json:"completed"`
+}
+
+var tasks []*Task = []*Task{}
+
 func main() {
+	tasks = append(tasks, &Task{ID: "1", Name: "eat", Completed: false})
+	tasks = append(tasks, &Task{ID: "2", Name: "sleep", Completed: false})
+
 	e := echo.New()
-	e.GET("/", hello)
+	e.GET("/tasks", getAllTasks)
 	e.Logger.Fatal(e.Start(":8080"))
 }
 
-func hello(c echo.Context) error {
-	return c.String(http.StatusOK, "hello world!!!")
+func getAllTasks(c echo.Context) error {
+	return c.JSON(http.StatusOK, tasks)
 }
